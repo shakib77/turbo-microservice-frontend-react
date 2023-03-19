a76e8dee4f0a90c4a29a04880cf42a0875c9f15e
shakib <shakib.shaon@softbdltd.com>
2023-03-19 23:54:47 +0600

    cleaning components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import './globals.css'
+import {ReactNode} from 'react'
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({children}: {children: ReactNode}) {
   return (
     <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <title>Tech Twig</title>
+      </head>
       <body>{children}</body>
     </html>
   )
